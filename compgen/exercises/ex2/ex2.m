19e564b4b18fba66ebe25310e860708ce3c2cf58
Sakari Cajanus <sakari.cajanus@aalto.fi>
2012-09-26 07:38:42 +0300

    compgen ex2 matlab files

--- compgen/exercises/ex2/ex2.m
+++ compgen/exercises/ex2/ex2.m
@@ -1,61 +1,39 @@
 %% Question 1
-HC22 = getgenbank('NT_011526', 'SequenceOnly', true);
-fprintf('Length: %d\n',length(HC22))
-fprintf('Ratio of chromosome 22: %.2f %%\n',length(HC22)/51304566*100)
+[score, ali] = swalign('NSRTYI','NFRTWI','scoringmatrix','blosum62')
+aas = 'NSRTYI'
+aaint = aa2int(aas)
+aas2 = 'NFRTWI'
+aaint2 = aa2int(aas2)
+m = blosum62();
+score = 0
+for i=1:length(aas)
+    score = score + m(aaint(i),aaint2(i))
+end
+score=score/2;
 
-% Most abundant nucleotide and dinucleotide:
-bc = basecount(HC22)
-dimercount(HC22)
-% GC-content:
-(bc.G+bc.C)/(bc.A+bc.G+bc.T+bc.C)
+bitscore = (0.31*score-log(0.038))/log(2)
+E = 0.038 * 5134782 * 30 * exp(-0.31*10)
 
 %% Question 2
-orfs = seqshoworfs(HC22(1:50000),'minimumlength',150);
-% => 9
-orfs = seqshoworfs(seqreverse(HC22(1:50000)),'minimumlength',150);
-% => 6
+aas = 'RKGA'
+aaint = aa2int(aas)
+m = blosum62();
+scores = zeros(4)
+for i=1:length(aas)
+    for j=1:length(aas)
+        scores(i,j) = m(aaint(i),aaint(j))
+    end
+end
 
-%% Question 3
-cpgi = cpgisland(HC22,'MinIsland', 200)
-[val, ind] = max(cpgi.Stops-cpgi.Starts)
-cpgi.Starts(13)
-cpgi.Stops(13)
+aas2 = 'VILM'
+aaint2 = aa2int(aas2)
+m = blosum62();
+scores = zeros(4)
+for i=1:length(aas)
+    for j=1:length(aas2)
+        scores(i,j) = m(aaint(i),aaint2(j))
+    end
+end
 
 %% Question 4
-% 2nd order Markov model based on HBB
-initial_distribution = zeros(16,1);
-freq_init = dimercount(HC22);
-fields = fieldnames(freq_init);
-for i=1:16
-    initial_distribution(i)=getfield(freq_init,fields{i});
-end
-initial_distribution = initial_distribution/sum(initial_distribution);
-
-
-transition_probabilities = zeros(4,16);
-[freq_trans, carray] = codoncount(HC22);
-fields_t = fieldnames(freq_trans);
-for i=1:64
-    transition_probabilities(i)=getfield(freq_trans,fields_t{i});
-end
-transition_probabilities=transition_probabilities';
-% now we have a transition matrix
-%     A   C   G   T
-% AA
-% AC
-% AG
-% AT etc.
-for i=1:16
-    transition_probabilities(i,:) = transition_probabilities(i,:)/...
-    sum(transition_probabilities(i,:));
-end
-
-seq_numeric = nt2int('gatacc');
-probability = initial_distribution((seq_numeric(1)-1)*4+seq_numeric(2));
-for i=3:length(seq_numeric)
-    probability = probability*transition_probabilities((seq_numeric(i-2)-1)*4...
-    +seq_numeric(i-1),seq_numeric(i));
-end
-logprob = log(probability);
-
-relfreq = seqwordcount(HC22, 'gatacc')/(length(HC22)-5)
+[score, alignment] = ex2_pr4('ATQWERI','ATQDSRI',blosum62(),-1)
---
+++ compgen/exercises/ex2/ex2_pr4.m
@@ -0,0 +1,76 @@
+function [score alignment] = ex2_pr4(seqA, seqB, scm, penalty)
+%EX2_PR4 Summary of this function goes here
+%   Detailed explanation goes here
+fprintf('%s\n',seqA)
+fprintf('%s\n',seqB)
+s1 = aa2int(seqA);
+s2 = aa2int(seqB);
+lengthA = length(seqA);
+lengthB = length(seqB);
+DPmatrix = zeros(lengthA+1, lengthB+1);
+pointer = zeros(lengthA+1, lengthB+1);
+max_score = 0;
+
+for i=2:lengthA+1
+    for j=2:lengthB+1
+        score_diag = DPmatrix(i-1,j-1) + scm(s1(i-1),s2(j-1));
+        score_up = DPmatrix(i,j-1) + penalty;
+        score_left = DPmatrix(i-1,j) + penalty;
+        DPmatrix(i,j)=max([0,score_diag,score_up,score_left]);
+        if(DPmatrix(i,j)==0)
+            pointer(i,j)=0;
+        end
+        if(DPmatrix(i,j)==score_left)
+            pointer(i,j)=1;
+        end
+        if(DPmatrix(i,j)==score_up)
+            pointer(i,j)=2;
+        end
+        if(DPmatrix(i,j)==score_diag)
+            pointer(i,j)=3;
+        end
+        if(DPmatrix(i,j)>=max_score)
+            max_i=i;
+            max_j=j;
+            max_score = DPmatrix(i,j);
+        end
+    end
+end
+ali1=''; ali2='';
+i = max_i; j = max_j;
+
+while pointer(i,j)~=0
+    if(pointer(i,j)==3)
+        ali1 = strcat(ali1, seqA(i-1));
+        ali2 = strcat(ali2, seqB(j-1));
+        i = i-1;
+        j = j-1;
+    elseif(pointer(i,j)==2)
+        ali1 = strcat(ali1,'-');
+        ali2 = strcat(ali2,seqB(j-1));
+        j = j-1;
+    elseif(pointer(i,j)==1)
+        ali1 = strcat(ali1, seqA(i-1));
+        ali2 = strcat(ali2, '-');
+        i = i-1;
+    end
+end
+
+ali1 = fliplr(ali1); ali2 = fliplr(ali2);
+i=0; j=0;
+score=0;
+
+for i=1:length(ali1)
+    if(ali1(i)=='-' || ali2(i)=='-')
+        score = score + penalty;
+    else
+        score = score + scm(aa2int(ali1(i)),aa2int(ali2(i)));
+    end
+end
+score=score/2;
+
+alignment(1,1:length(ali1))=ali1;
+alignment(2,1:length(ali2))=ali2;
+
+end
+
