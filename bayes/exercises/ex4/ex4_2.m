056fa3a5ca7c0b2ea11b0124692042a697ace9ad
Sakari Cajanus <sakari.cajanus@aalto.fi>
2012-11-11 18:55:19 +0200

    Added Bayes ex4 stuff

---
+++ bayes/exercises/ex4/ex4_2.m
@@ -0,0 +1,12 @@
+%   Author: Jaakko Riihimäki <jaakko.riihimaki@tkk.fi>
+%   Last modified: 2007-10-10 09:46:22 EEST
+
+%
+% This is a template file for the exercise 4.2 in the 
+% course S-114.2601 Introduction to Bayesian Modeling
+
+
+% Bioassay data, (book page .89)
+x=[-.86 -.30 -.05 .73]';
+n=[5 5 5 5]';
+y=[0 1 3 5]';
---
+++ bayes/exercises/ex4/ex5_1.m
@@ -0,0 +1,14 @@
+%   Author: Jaakko Riihimäki <jaakko.riihimaki@tkk.fi>
+%   Last modified: 2007-10-16 12:24:23 EEST
+
+%
+% This is a template file for the exercise 5.1 in the
+% course S-114.2601 Introduction to Bayesian Modeling
+
+
+% SAT-example data (book. p. 140)
+% y is estimated treatment effect
+% s is standard error of effect estimate
+y=[28 8 -3 7 -1 1 18 12];
+s=[15 10 16 11 9 11 10 18];
+
